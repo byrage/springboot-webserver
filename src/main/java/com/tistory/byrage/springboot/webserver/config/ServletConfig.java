7f9563a449c73eeff223185a09f916d80d32933c
byrage <byrage91@gmail.com>
2020-02-01 23:41:00 +0900

    Add GenericServlet

--- src/main/java/com/tistory/byrage/springboot/webserver/config/ServletConfig.java
+++ src/main/java/com/tistory/byrage/springboot/webserver/config/ServletConfig.java
@@ -1,15 +1,21 @@
 package com.tistory.byrage.springboot.webserver.config;
 
+import com.tistory.byrage.springboot.webserver.servlet.CalculatorServlet;
 import com.tistory.byrage.springboot.webserver.servlet.HelloServlet;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.*;
 
 @Configuration
 public class ServletConfig {
 
     @Bean
-    public ServletRegistrationBean<HelloServlet> servletRegistrationBean() {
+    @Primary
+    public ServletRegistrationBean<CalculatorServlet> calculatorServletRegistrationBean() {
+        return new ServletRegistrationBean<>(new CalculatorServlet());
+    }
+
+    @Bean
+    public ServletRegistrationBean<HelloServlet> helloServletRegistrationBean() {
         return new ServletRegistrationBean<>(new HelloServlet());
     }
 }
