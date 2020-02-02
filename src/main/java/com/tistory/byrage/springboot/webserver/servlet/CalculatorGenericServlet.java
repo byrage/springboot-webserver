790b191db8456c2c874bf13aa6ad39c87a84e906
byrage <byrage91@gmail.com>
2020-02-02 21:10:29 +0900

    Rename CalculatorServlets

--- src/main/java/com/tistory/byrage/springboot/webserver/servlet/CalculatorGenericServlet.java
+++ src/main/java/com/tistory/byrage/springboot/webserver/servlet/CalculatorGenericServlet.java
@@ -2,24 +2,22 @@ package com.tistory.byrage.springboot.webserver.servlet;
 
 import lombok.extern.slf4j.Slf4j;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.*;
+import javax.servlet.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 
 @Slf4j
-@WebServlet
-public class CalculatorHttpServlet extends HttpServlet {
+public class CalculatorGenericServlet extends GenericServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+
         int num1 = Integer.parseInt(req.getParameter("num1"));
         int num2 = Integer.parseInt(req.getParameter("num2"));
 
-        resp.setContentType("text/plain");
+        res.setContentType("text/plain");
 
-        PrintWriter writer = resp.getWriter();
+        PrintWriter writer = res.getWriter();
         writer.println("num1=" + num1 + ", num2=" + num2);
         writer.println(num1 + " + " + num2 + " = " + (num1 + num2));
     }
--- src/main/java/com/tistory/byrage/springboot/webserver/servlet/CalculatorServlet.java
+++ src/main/java/com/tistory/byrage/springboot/webserver/servlet/CalculatorServlet.java
@@ -2,22 +2,24 @@ package com.tistory.byrage.springboot.webserver.servlet;
 
 import lombok.extern.slf4j.Slf4j;
 
-import javax.servlet.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 
 @Slf4j
-public class CalculatorServlet extends GenericServlet {
+@WebServlet("/calc")
+public class CalculatorServlet extends HttpServlet {
 
     @Override
-    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
-
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         int num1 = Integer.parseInt(req.getParameter("num1"));
         int num2 = Integer.parseInt(req.getParameter("num2"));
 
-        res.setContentType("text/plain");
+        resp.setContentType("text/plain");
 
-        PrintWriter writer = res.getWriter();
+        PrintWriter writer = resp.getWriter();
         writer.println("num1=" + num1 + ", num2=" + num2);
         writer.println(num1 + " + " + num2 + " = " + (num1 + num2));
     }
